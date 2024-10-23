da8affb0745e72f8b5cda1fd4e5e69ac2934cbf3
samuel6814 <samuelquaigraine6814@gmail.com>
2024-10-23 19:35:43 +0000

    made progress

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+const SearchBar = () => {
+    return (
+        <form className="">
+            <input type="text" placeholder="search"/>
+            <button className="cursor-pointer"></button>
+        </form>
+    )
+}
+
+export default SearchBar
