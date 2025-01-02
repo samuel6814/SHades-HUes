9af27c7d352a76cef86c31d5ba146911e9295302
samuel6814 <samuelquaigraine6814@gmail.com>
2025-01-02 22:25:54 +0000

    Major Frontend Designs Completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,34 @@
+"use client"
+
 import CategoryList from "@/components/CategoryList"
 import ProductList from "@/components/ProductList"
 import Slider from "@/components/Slider"
+import { WixClientContext } from "@/context/wixContext"
+import { useWixClient } from "@/hooks/useWixClient"
+import { wixClientServer } from "@/lib/wixClientServer"
+import { useContext, useEffect } from "react"
 
-const HomePage = () => {
+const HomePage = async () => {
+   
+//   const wixClient = useWixClient()
+
+// useEffect(() => {
+
+  
+//   const getProduct = async () => { 
+//     const res = await wixClient.products.queryProducts().find();
+ 
+//     console.log(res)
+//   };
+//   getProduct()
+// },[wixClient]
+// )
+const wixClient = await wixClientServer();  
+
+const res = await wixClient.products.queryProducts().find();
+
+console.log(res)
+
   return (
     <div className=''>
       <Slider/>
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,34 +1,38 @@
-"use client"
+"use client";
 
-import Image from "next/image"
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 const SearchBar = () => {
 
-    const router = useRouter();
+  const router = useRouter();
 
-    const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        const formData = new FormData(e.currentTarget);
-        const name = formData.get("name") as string;
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const name = formData.get("name") as string;
 
-        if(name){
-          router.push(`/list?name=${"name"}`)  
-        }
+    if(name){
+      router.push(`/list?name=${name}`)
+    }
+  };
 
+  return (
+    <form
+      className="flex items-center justify-between gap-4 bg-gray-100 p-2 rounded-md flex-1"
+      onSubmit={handleSearch}
+    >
+      <input
+        type="text"
+        name="name"
+        placeholder="Search"
+        className="flex-1 bg-transparent outline-none"
+      />
+      <button className="cursor-pointer">
+        <Image src="/search.png" alt="" width={16} height={16} />
+      </button>
+    </form>
+  );
+};
 
-    };
-
-    return (
-        <form className="flex ic justify-between gap-4 bg-gray-100 p-2 rounded-md flex-1 " onSubmit={handleSearch}>
-
-        
-
-            <input type="text" placeholder="search" className="flex-1 bg-transparent outline-none" />
-            <button className="cursor-pointer"></button>
-            <Image src="/search.png" alt="" width={20} height={16}/>
-        </form>
-    )
-}
-
-export default SearchBar
+export default SearchBar;
